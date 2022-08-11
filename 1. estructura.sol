645cda9b33c5b971ed0478df0fd50f518a78a46f
jorge zubieta <zubietajor@gmail.com>
2022-08-11 13:40:41 -0300

    estructura pragma, contract, y constructor

---
+++ 1. estructura.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL - 3.0
+
+//el comentario de la fila 1 es necesario!
+// GPL -> General Public License!
+
+pragma solidity >=0.7.0 <0.9.0;
+    // rango maximo de 2 versiones
+    // Ej entre 7 y 9
+    // importante : el nombre del contrato debe ser el mismo que el nombre del archivo
+
+contract Estructura {
+// Declaración de un contrato, similar a una clase
+// contract <Name> { Name puede ser cualquier cosa
+// 1 contrato por archivo
+
+    constructor(){
+        // funcion opcional que se realiza solo una vez
+    }
+}
